e9a930d7341c4b9406ed316d8daa85f5668471a8
DMunkei <stareye863@gmail.com>
2020-12-04 00:38:35 +0100

    Quicksort works now. Veri nais

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,94 +1,83 @@
 #include <iostream>
 
-void swap(int* a, int p1, int p2)
+void swap(int* p1, int* p2)
 {
-	int tmp = *(a+p1);
-	*(a+p1) = *(a+p2);
-	*(a+p2) = tmp;
+	int tmp = *p1;
+	*p1 = *p2;
+	*p2 = tmp;
 }
 
 int getPivot(int* a)
 {	
 	return (sizeof(a) / sizeof(int)) / 2;
 }
-
-void quicksort(int* a, int low, int high)
+int partition(int* a, int low, int high)
 {
 	//base case: it's already sorted if there's only one element.
-	if(high - low <= 1)
-	{
-		return;
-	}
 
 	int pivot = getPivot(a);
 	int lWalker = low;
 	int rWalker = high;
-	//put the pivot to the end of the array
-	for(int i=0; i<7;i++)
-	{
-		std::cout<< a[i] << ", ";
-		if(i==6)
-		{
-			std::cout<<std::endl;
-		}
-	}
-	swap(a, pivot, high);
-	std::cout << lWalker << ": LW - RW : " << rWalker << std::endl;
-	std::cout <<"PIVOT: " << pivot << std::endl; 
-	std::cout <<"LOW: " << low << std::endl; 
-	std::cout <<"HIGH: " << high << std::endl; 
-	for(int i=0; i<7;i++)
-	{
-		std::cout<< a[i] << ", ";
-	}
-	std::cin.get();
+
+	swap(&a[pivot], &a[high]);
+//	std::cout << lWalker << ": LW - RW : " << rWalker << std::endl;
+//	std::cout <<"PIVOT: " << pivot << std::endl; 
+//	std::cout <<"LOW: " << low << std::endl; 
+//	std::cout <<"HIGH: " << high << std::endl; 
 	do
 	{
 //		std::cout << "stuck here?" << std::endl;
-		while(lWalker <= high && *(a+lWalker) < *(a+high))
+		while(lWalker <= high && a[lWalker] < a[high])
 		{
-			std::cout<< "LW: " << lWalker << std::endl;
 			lWalker++;
 		}
-		while(rWalker >= low && *(a+rWalker) > *(a+high))
+		while(rWalker >= low && a[rWalker] > a[high])
 		{
-			std::cout<< "RW: " << rWalker << std::endl;
 			rWalker--;
 
 		}
-		swap(a,*(a+lWalker), *(a+rWalker));
+		swap(&a[lWalker], &a[rWalker]);
 
 		//swap the pivot element to its proper position
 		if(rWalker <= lWalker)
 		{
-			swap(a,lWalker,high);
+			swap(&a[lWalker],&a[high]);
 		}
 
-		std::cout << lWalker << ": LW - RW : " << rWalker << std::endl;
 	}
-	while(lWalker <= rWalker);
-	
+	while(lWalker < rWalker);
+	return lWalker;
+}
 
-	quicksort(a, low, pivot);
+void quicksort(int* a, int low, int high)
+{
+	if((high - low) <= 1 )
+	{
+		return;
+	}
+	int pivot = partition(a,low,high);
+
+	quicksort(a, low, pivot-1);
 	quicksort(a, pivot+1,high);
 
 }
 
 
 
+void showArray(int* a,int n){
+	for(int i =0; i<n;i++)
+	{
+		std::cout<< a[i] << "\t";
+	}
+}
 int main()
 {
-	int a[] = {3,4,5,1,2,6,8,7};
-
-//	for(int i = 0; i<7; i++)
-//	{
-//		std::cout<< a[i] << ", ";
-//	}
-	quicksort(a,0,7);
-
-	for(int i = 0; i<7; i++)
-	{
-		std::cout<< a[i] << ", ";
-	}
+	int a[] = {3,4,5,1,2,6,8,7,420,99,0,674,300};
+	int n = sizeof(a) /sizeof(int);
+	std::cout<<"pre"<<std::endl;
+	showArray(a,n);
+	quicksort(a,0,n-1);
+	std::cout<<"post"<<std::endl;
+	showArray(a,n);
 	 return 0;
 }
